068e4ce5ed98b82753fa51691e3041b716a2f913
Epersonal <92110978+EvanM07@users.noreply.github.com>
2023-08-02 09:34:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mock_up_one
+ A Mock up for a website I am working on 
---
+++ javascript/js_main.js
@@ -0,0 +1,9 @@
+// These functions open and close the contact form
+function openForm() {
+    document.getElementsByClassName("contact-form")[0].style.display = "block";
+}
+
+function closeForm() {
+    document.getElementsByClassName("contact-form")[0].style.display = "none";
+}
+
